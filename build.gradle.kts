8984796f583bba10576e16006426c700791a6e6e
Erik R. Yverling <erik.r@yverling.se>
2024-05-29 16:34:50 +0200

    Filter out non stable versions with Versions with Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,12 @@ application {
     mainClass.set("se.yverling.lab.kotlin.MainKt")
 }
 
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
+}
+
 fun isNonStable(version: String): Boolean {
     val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
     val regex = "^[0-9,.v-]+(-r)?$".toRegex()
