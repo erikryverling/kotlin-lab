c571d213668c1239265465a971787e632ec96feb
Erik R. Yverling <erik.r@yverling.se>
2024-11-29 15:59:55 +0100

    Bump dependencies

--- src/main/kotlin/se/yverling/lab/kotlin/Main.kt
+++ src/main/kotlin/se/yverling/lab/kotlin/Main.kt
@@ -1,12 +1,8 @@
 package se.yverling.lab.kotlin
 
-import se.yverling.lab.kotlin.coroutines.CoroutinesBench
 import se.yverling.lab.kotlin.flow.FlowBench
-import se.yverling.lab.kotlin.kotlin.KotlinBench
 
 fun main() {
-    KotlinBench.all()
-    CoroutinesBench.all()
-    FlowBench.all()
+    FlowBench.`Combining flows`()
 }
 
